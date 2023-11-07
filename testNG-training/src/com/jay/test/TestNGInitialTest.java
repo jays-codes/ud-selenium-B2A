630c0af048ac6acf628965a9221f8e2a8c257154
jay menorca <zaimenorca@gmail.com>
2023-11-07 09:18:07 -0500

    updated gitignore for test-output folder;

---
+++ testNG-training/src/com/jay/test/TestNGInitialTest.java
@@ -0,0 +1,16 @@
+package com.jay.test;
+
+import org.testng.annotations.Test;
+
+public class TestNGInitialTest {
+
+	@Test
+	public void tcDemo() {
+		System.out.println("Hello darkness my old friend....");
+	}
+	
+	@Test
+	public void tcCase2() {
+		System.out.println("Test case #2");
+	}
+}
