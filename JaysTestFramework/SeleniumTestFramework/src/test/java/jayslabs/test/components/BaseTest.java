56f9cd39a3471ae5e9f837c11d5f302771440acd
jay menorca <zaimenorca@gmail.com>
2023-11-14 19:23:35 -0500

    182; passing param from cli;

--- JaysTestFramework/SeleniumTestFramework/src/test/java/jayslabs/test/components/BaseTest.java
+++ JaysTestFramework/SeleniumTestFramework/src/test/java/jayslabs/test/components/BaseTest.java
@@ -4,7 +4,6 @@ import org.testng.annotations.AfterMethod;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.Timestamp;
@@ -20,6 +19,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -35,31 +35,25 @@ public class BaseTest {
 	public WebDriver driver;
 	public LandingPage landingPage;
 
-	public WebDriver initializeDriver() {
-//		if (driver!=null) {
-//			System.out.println("driver not null...");
-//			return driver;
-//		}
-//		
+	public WebDriver initializeDriver() throws IOException {
+
 		Properties prop = new Properties();
-		try {
 
-			String fstr = System.getProperty("user.dir") + "\\src\\main\\resources\\GlobalData.properties";
-			FileInputStream fis = new FileInputStream(fstr);
+		String fstr = System.getProperty("user.dir") + "\\src\\main\\resources\\GlobalData.properties";
+		FileInputStream fis = new FileInputStream(fstr);
 
-			prop.load(fis);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String browsername = prop.getProperty("browser");
+		prop.load(fis);
+		// browsername = prop.getProperty("browser");
+
+		String browsername = System.getProperty("browser") != null ? System.getProperty("browser")
+				: prop.getProperty("browser");
 
 		if (browsername.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		} else if (browsername.equalsIgnoreCase("firefox")) {
-
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
 		}
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -69,7 +63,7 @@ public class BaseTest {
 	}
 
 	@BeforeMethod(alwaysRun = true)
-	public LandingPage launchApplication() {
+	public LandingPage launchApplication() throws IOException {
 		driver = initializeDriver();
 		landingPage = new LandingPage(driver);
 		landingPage.goTo();
@@ -79,33 +73,28 @@ public class BaseTest {
 	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		driver.quit();
-		//driver.close();
+		// driver.close();
 	}
-	
+
 	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
-		
-		String jsonstr = FileUtils
-				.readFileToString(
-						new File(filePath), StandardCharsets.UTF_8);
-		
-		//using jackson databind to convert json str to HashMap
+
+		String jsonstr = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+
+		// using jackson databind to convert json str to HashMap
 		ObjectMapper mapper = new ObjectMapper();
-		List<HashMap<String, String>> data 
-			= mapper.readValue(
-					jsonstr, 
-					new TypeReference<List<HashMap<String, String>>>(){
-		});
+		List<HashMap<String, String>> data = mapper.readValue(jsonstr,
+				new TypeReference<List<HashMap<String, String>>>() {
+				});
 		return data;
 	}
-	
+
 	public String takeScreenshot(String tcname, WebDriver driver2) throws IOException {
 		TakesScreenshot ts = (TakesScreenshot) driver2;
 		File src = ts.getScreenshotAs(OutputType.FILE);
-		
+
 		String fname = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 		String fstr = System.getProperty("user.dir") + "\\test-output\\screenshots\\" + tcname + fname + ".png";
 
-		
 		File dest = new File(fstr);
 		FileUtils.copyFile(src, dest);
 		return fstr;
