b81c9fdf27bf8aa88369080ddb63cf70ca06dc1f
jay menorca <zaimenorca@gmail.com>
2023-11-17 15:35:30 -0500

    193; called cucumber profile via mvn and jenkins

--- JaysTestFramework/SeleniumTestFramework/src/test/java/jayslabs/cucumber/TestCucumberRunnerTest.java
+++ JaysTestFramework/SeleniumTestFramework/src/test/java/jayslabs/cucumber/TestCucumberRunnerTest.java
@@ -1,15 +1,11 @@
 package jayslabs.cucumber;
 
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
-//@CucumberOptions(features="src/test/java/jayslabs/cucumber", 
-//glue="jayslabs.cucumber.stepDefinitions",
-// monochrome=true, tags= "@Regression and @Error", plugin= {"html:target/cucumber.html"})
 
 @CucumberOptions(features="src/test/java/jayslabs/cucumber", 
 glue="jayslabs.cucumber.stepDefinitions",
  monochrome=true, plugin= {"html:target/cucumber.html"})
-public class TestNGRunner extends AbstractTestNGCucumberTests {
-	
+public class TestCucumberRunnerTest extends AbstractTestNGCucumberTests {
 }
