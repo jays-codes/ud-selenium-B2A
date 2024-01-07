50e502c30616781dac8e08eaf97af7c3770f4fdf
jay menorca <zaimenorca@gmail.com>
2024-01-07 08:19:05 -0500

    updated jackson databind to new version

---
+++ JaysTestFramework/SeleniumTestFramework/src/test/java/jayslabs/tester/RestAssuredTester.java
@@ -0,0 +1,15 @@
+package jayslabs.tester;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class RestAssuredTester {
+
+	public static void main(String[] args) {
+		RestAssured.baseURI = "";
+		
+		System.out.println("yow");
+
+	}
+
+}
---
+++ JaysTestFramework/SeleniumTestFramework/src/test/java/jayslabs/tester/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package jayslabs.tester;
